14580479c139a06a20734ad12dc3ae40662ffae6
Jyoti <Jyotiagar012@gmail.com>
2023-10-25 09:14:31 -0700

    new2

---
+++ get.js
@@ -0,0 +1,16 @@
+//alert("hello")
+//let data=document.getElementById("a").innerText;
+
+//let data=document.getElementById("a").innerHTML;
+
+let data=document.getElementById("a").getAttribute("style")
+
+//let data=document.getElementById("a").getAttributeNode("style")
+
+//let data=document.getElementById("a").attributes[0]
+
+
+
+
+
+console.log(data);
---
+++ set.js
@@ -0,0 +1,6 @@
+//document.write("hell")
+
+document.getElementById("a").innerHTML ="<center><h2>PNINFOSY WGALIOR 123</h2>";
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+document.getElementById("a").removeAttribute("style");
+document.getElementById("b").innerHTML ="<h1>GWALIOR</h2>";
